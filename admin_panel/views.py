c90cbe9e1bcf8cdffd22c89359be099261dc6cd3
dhil-shad <dhilshadmuhammed@outlook.com>
2026-02-28 00:19:19 +0530

    added notification to mail

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from accounts.models import BusDetails
 from routes.models import Route, Location, RouteTemplate
 from bookings.models import Booking
+from django.core.mail import send_mail
+from django.conf import settings
 from .serializers import (
     UserAdminSerializer,
     BusDetailsAdminSerializer,
@@ -22,6 +24,24 @@ class BusDetailsViewSet(viewsets.ModelViewSet):
     queryset = BusDetails.objects.all().order_by('-id')
     serializer_class = BusDetailsAdminSerializer
     permission_classes = [IsAdminUser]
+
+    def perform_update(self, serializer):
+        instance = self.get_object()
+        old_status = instance.status
+        updated_instance = serializer.save()
+        
+        if old_status != 'approved' and updated_instance.status == 'approved':
+            try:
+                user_email = updated_instance.user.email
+                username = updated_instance.user.username
+                bus_name = updated_instance.bus_name
+                
+                subject = "Bus Registration Approved - TravelSync"
+                message = f"Hello {username},\n\nYour bus registration for '{bus_name}' has been approved by the admin! You can now log in to the TravelSync bus operator panel.\n\nThank you,\nTravelSync Team"
+                
+                send_mail(subject, message, settings.EMAIL_HOST_USER, [user_email], fail_silently=True)
+            except Exception as e:
+                print(f"Failed to send approval email: {e}")
 
 class LocationViewSet(viewsets.ModelViewSet):
     queryset = Location.objects.all().order_by('name')
